380400175234cd8c94a21fb28f4554b165db4d55
Allan Carlos <allancarlosdeveloper@gmail.com>
2019-03-16 08:40:16 -0300

    web: initial commit

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
