b434f23ab99e71d9fa6ba8ebd10e24f8f3852673
Allan Carlos <allancarlosdeveloper@gmail.com>
2019-04-02 19:25:11 -0300

    refactor: change ip

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const token = "e0e6e2967dde85c9b0c604989ae1af47";
 
-axios.defaults.baseURL = "http://127.0.0.1:3333/api/v1";
+axios.defaults.baseURL = "http://192.168.0.102:3333/api/v1";
 // axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 export default {
