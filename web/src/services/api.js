8e9c7ac908077ddb5897f2927ba390c645d21b62
Allan Carlos <allancarlosdeveloper@gmail.com>
2019-04-04 18:50:17 -0300

    feat: API address with https

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const token = "e0e6e2967dde85c9b0c604989ae1af47";
 
-axios.defaults.baseURL = "http://api.terapias.me/v1";
+axios.defaults.baseURL = "https://api.terapias.me/v1";
 // axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 export default {
