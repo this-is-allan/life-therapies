0f91aa0335127481a19b5e14bf86cf13a17a9078
Allan Carlos <allancarlosdeveloper@gmail.com>
2019-04-05 09:02:14 -0300

    debug

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_HOST;
 
+console.log(process.env);
+
 export default {
   therapies: {
     fetchTherapy: id => axios.get(`/therapies/${id}`).then(res => res.data),
